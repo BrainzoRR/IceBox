b33aaf6cd3fd1de3f89853fe6e3b99226cd84868
BrainzoRR <134707147+BrainzoRR@users.noreply.github.com>
2026-01-13 20:57:17 +0300

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ load_dotenv()
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 BOT_USERNAME = "IceBoxTbot"
-YOOKASSA_SHOP_ID = os.getenv("1245114")
-YOOKASSA_SECRET_KEY = os.getenv("live_c9qqTa2V87xWHomMI-i2m6XQZI0_eu3J7Dyz7aVZAx8")
+
 DB_PATH = "icebox.db"
+
 FREE_LIMIT = 50
